c1f9e39cd12f31b421e25b4fb51280fe68e93996
junbn <junbin01217@gmail.com>
2020-09-01 16:56:56 +0800

    i dont know what URL that we need to map,so i put input first,then i also not too sure how to change the thing to store in the JSON

--- junbin part.py
+++ junbin part.py
@@ -1,1 +1,11 @@
-print('hi')
+import scrapy
+      class spidery(scrapy.spider):
+           name = "spidery"
+           start_urls = ["input"]
+           def parse(self,response):
+                     css selector = 'img'
+                     for x in response.css(css_selector):
+                              newsel = '@src'
+                              yield{
+                                  "image Link":x.xpath(newsel).extract_first(),
+                              }
