7449bf5baf2f41dee9499d3d57addaa332b26de5
junbn <junbin01217@gmail.com>
2020-09-06 15:47:32 +0800

    i double checking

--- scrapytest.py
+++ scrapytest.py
@@ -1,11 +1,20 @@
-import scrapy
-      class spidery(scrapy.spider):
-           name = "spidery"
-           start_urls = ["input"]
-           def parse(self,response):
-                     css selector = 'img'
-                     for x in response.css(css_selector):
-                              newsel = '@src'
-                              yield{
-                                  "image Link":x.xpath(newsel).extract_first(),
-                              }
+class Spidery(scrapy.spider):
+    name = "spidery"
+    start_urls = ['http://172.18.58.238/index.php']
+    def parse(self, response):
+        xpath_selector ='//img'
+        for x in response.xpath(xpath_selector):
+            newsel ='@src'
+            yield{
+                  'image Link':x.xpath(newsel).extract_first(),
+
+            }
+
+        #To recurse next page
+    Page_selector = '.next a ::attr(href)'
+        name_page = response.css(page_selector).extract_first()
+        if next_page:
+            yield scrappy.Request(
+                  response.urljoin(next_page),
+                  callback=self.parse
+            )
